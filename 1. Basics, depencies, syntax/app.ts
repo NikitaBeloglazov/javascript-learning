151c7ec112ae4227c3d0400828c971b4ee748c61
Nikita Beloglazov <77229847+NikitaBeloglazov@users.noreply.github.com>
2025-08-27 15:40:18 +0300

    Standartizing currency requests to make_request, add unix_timestamp_to_data

--- 1. Basics, depencies, syntax/app.ts
+++ 1. Basics, depencies, syntax/app.ts
@@ -1,11 +1,34 @@
 import fetch from 'node-fetch';
 
-async function main() {
-	const UAH_response = await fetch('https://api.fxratesapi.com/latest?base=UAH');
-	const UAH_data = await UAH_response.json();
+async function unix_timestamp_to_data(unix_timestamp: number) {
+	//
 
-	console.log(UAH_data["date"]);
-	console.log(`The newest courses are dated as: ${UAH_data["date"]}`);
+	// Create a new JavaScript Date object based on the timestamp
+	// multiplied by 1000 so that the argument is in milliseconds, not seconds
+	var date = new Date(unix_timestamp);
+
+	return date.toLocaleString();
+}
+
+async function make_request(currency: string) {
+	var request_response = await fetch(`https://api.fxratesapi.com/latest?base=${currency}`)
+	var request_data = await request_response.json()
+	if (request_data["success"] == false) {
+		console.log(request_data)
+		throw new Error(`API service said that request for currency "${currency}" was unsucceful. Try again later`)
+	}
+	else {
+		return request_data
+	}
+}
+
+async function main() {
+	// let return_text = ""
+
+	var UAH_data = await make_request('UAH')
+
+	console.log(UAH_data)
+	console.log(`The newest courses are dated as: ${await unix_timestamp_to_data(UAH_data["date"])}`)
 
 	// const EUR_response = await fetch('https://api.fxratesapi.com/latest?base=EUR');
 	// const EUR_data = await EUR_response.json();
@@ -17,7 +40,8 @@ async function main() {
 
 	// console.log(USD_data);
 
-	return '1';
+	// console.log(return_text)
+	return null
 }
 
 main();
