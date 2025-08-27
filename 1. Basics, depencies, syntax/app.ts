fa1b82b9a62dc62a2a0d18a42014252a04ddbe9e
Nikita Beloglazov <77229847+NikitaBeloglazov@users.noreply.github.com>
2025-08-27 14:08:00 +0300

    Fix modules import

--- 1. Basics, depencies, syntax/app.ts
+++ 1. Basics, depencies, syntax/app.ts
@@ -1,1 +1,12 @@
-console.log('Hello, World');
+import fetch from 'node-fetch';
+
+async function main() {
+	const response = await fetch('https://api.github.com/users/github');
+	const data = await response.json();
+
+	console.log(data);
+
+	return 'Return';
+}
+
+main();
