a2c289dd415b18e9ff2eef156612a1023779dc0b
Nikita Beloglazov <77229847+NikitaBeloglazov@users.noreply.github.com>
2025-08-27 14:36:27 +0300

    Learning how to work with json

--- 1. Basics, depencies, syntax/app.ts
+++ 1. Basics, depencies, syntax/app.ts
@@ -1,12 +1,23 @@
 import fetch from 'node-fetch';
 
 async function main() {
-	const response = await fetch('https://api.github.com/users/github');
-	const data = await response.json();
+	const UAH_response = await fetch('https://api.fxratesapi.com/latest?base=UAH');
+	const UAH_data = await UAH_response.json();
 
-	console.log(data);
+	console.log(UAH_data["date"]);
+	console.log(`The newest courses are dated as: ${UAH_data["date"]}`);
 
-	return 'Return';
+	// const EUR_response = await fetch('https://api.fxratesapi.com/latest?base=EUR');
+	// const EUR_data = await EUR_response.json();
+
+	// console.log(EUR_data);
+
+	// const USD_response = await fetch('https://api.fxratesapi.com/latest?base=USD');
+	// const USD_data = await USD_response.json();
+
+	// console.log(USD_data);
+
+	return '1';
 }
 
 main();
