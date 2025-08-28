77bca39ff3ddece7d7514fc5afdb847b99fbb13d
Nikita Beloglazov <77229847+NikitaBeloglazov@users.noreply.github.com>
2025-08-28 15:52:35 +0300

    Remake to classes, making progress to Dependency Inversion

--- 1. Basics, depencies, syntax/app.ts
+++ 1. Basics, depencies, syntax/app.ts
@@ -1,8 +1,52 @@
 import fetch from 'node-fetch';
+import { setTimeout } from 'timers/promises';
+
+class Currency {
+	// unix_latest_refresh: number = 0
+	// rates: Object = {} // JSON with trade rates
+
+	constructor(
+		public name: String,
+		public rates: Object, // JSON with trade rates
+		public unix_latest_refresh: number,
+	) { }
+
+}
+
+class CurrencyController {
+	currencies: Object = {} // JSON with currencies classes
+
+	public async get_rates(currency_name: string) {
+		// console.log(this.currencies)
+		if (currency_name in this.currencies) {
+			console.log(`Returning up-to-date currency: ${currency_name}`)
+			return this.currencies[currency_name] // if currency already fetched
+			// TODO latest refresh check
+		}
+		else {
+			console.log(`Refreshing currency: ${currency_name}`)
+			await this.__refresh_rate(currency_name)
+			// console.log(this.currencies)
+			return this.currencies[currency_name]
+		}
+	}
+
+	private async __refresh_rate(currency_name: string) {
+		// Getting rates json
+		const request = await make_request(currency_name)
+		// console.log(request)
+		const rates_json = request["rates"]
+		const unix_latest_refresh = request["timestamp"] // already in unixtime
+
+		this.currencies[currency_name] = new Currency(currency_name, rates_json, unix_latest_refresh)
+	}
+
+}
+
 
 async function unix_timestamp_to_data(unix_timestamp: number) {
 	// Convert UNIX timestamp to human readable format (27/08/2025, 16.00.00)
-	const date = new Date(unix_timestamp);
+	const date = new Date(unix_timestamp * 1000); // cause Date() uses non-unixtime for some reason // TODO make more compact?
 	return date.toLocaleString();
 }
 
@@ -18,28 +62,38 @@ async function make_request(currency: string) {
 	}
 }
 
-async function main() {
-	let return_text = "- = Exchange rates\n"
-
-	const UAH_data = await make_request('UAH')
-
-	// console.log(UAH_data)
+async function exchange_rates_UI() {
 	// console.log()
-	return_text = return_text + `Server latest refresh: ${await unix_timestamp_to_data(UAH_data["date"])}\n\n`
 
-	const EUR_data = await make_request('EUR')
-	console.log(EUR_data);
+	let return_text = "\n\n- = Exchange rates\n"
 
-	const USD_data = await make_request('USD');
-	console.log(USD_data);
+	const UAH_data = await currency_controller.get_rates("UAH")
+	const EUR_data = await currency_controller.get_rates("EUR")
+	const USD_data = await currency_controller.get_rates("USD")
+
+	return_text = return_text + `Server latest refresh: ${await unix_timestamp_to_data(UAH_data.unix_latest_refresh)}\n\n`
 
 	return_text = return_text + "- = Exchange course for UAH = -"
 	return_text = return_text + `\n\nEUR:\nBuy: ${(1 / UAH_data["rates"]["EUR"]).toFixed(2)}\nSell: ${EUR_data["rates"]["UAH"].toFixed(2)}` // TODO
 
 	return_text = return_text + `\n\nUSD:\nBuy: ${(1 / UAH_data["rates"]["USD"]).toFixed(2)}\nSell: ${USD_data["rates"]["UAH"].toFixed(2)}` // TODO
+	return_text = return_text + `\n\n`
 
 	console.log(return_text)
 	return null
 }
 
-main();
+const currency_controller = new CurrencyController()
+
+async function main() {
+	while (true) {
+		await exchange_rates_UI()
+
+		for (let i = 0; i < 21; i++) { // refresh every 20 secs for cache check
+			process.stdout.write(`Waiting ${i}/20 secs\r\r`);
+			await setTimeout(1000) // ms
+		}
+	}
+}
+
+main() // trough main bcoz [typescript] Top-level 'await' expressions are only allowed when the 'module' option is set to ...
