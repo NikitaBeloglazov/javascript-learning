01f1aead8d68c3499672eead35aa72bbf352b61d
Nikita Beloglazov <77229847+NikitaBeloglazov@users.noreply.github.com>
2025-08-30 16:28:33 +0300

    Add currency cache and cache outdating

--- 1. Basics, depencies, syntax/app.ts
+++ 1. Basics, depencies, syntax/app.ts
@@ -15,24 +15,33 @@ class Currency {
 
 class CurrencyController {
 	currencies: Object = {} // JSON with currencies classes
+	allowed_time_laxity: number = -120 // 120 seconds allowed, after that - refresh
 
 	public async get_rates(currency_name: string) {
 		// console.log(this.currencies)
-		if (currency_name in this.currencies) {
-			console.log(`Returning up-to-date currency: ${currency_name}`)
-			return this.currencies[currency_name] // if currency already fetched
-			// TODO latest refresh check
+		if (currency_name in this.currencies) { // if currency already fetched
+			const data_age = this.currencies[currency_name]["unix_latest_refresh"] - unix_time.get_now()
+			if (data_age < this.allowed_time_laxity) {
+				console.log(`Refreshing outdated currency: ${currency_name} (Age: ${data_age})`)
+				await this.__refresh_rate(currency_name)
+				return this.currencies[currency_name]
+			}
+			else { // if up-to-date
+				console.log(`Returning up-to-date currency: ${currency_name} (Age: ${data_age})`)
+				return this.currencies[currency_name]
+			}
+
 		}
 		else {
-			console.log(`Refreshing currency: ${currency_name}`)
+			console.log(`Getting new currency: ${currency_name}`)
 			await this.__refresh_rate(currency_name)
-			// console.log(this.currencies)
 			return this.currencies[currency_name]
 		}
 	}
 
 	private async __refresh_rate(currency_name: string) {
 		// Getting rates json
+		console.log(`FETCH: ${currency_name}`)
 		const request = await make_request(currency_name)
 		// console.log(request)
 		const rates_json = request["rates"]
@@ -43,12 +52,21 @@ class CurrencyController {
 
 }
 
+class UnixTime {
+	public async to_data(unix_timestamp: number) {
+		// Convert UNIX timestamp to human readable format (27/08/2025, 16.00.00)
+		console.log(unix_timestamp)
+		const date = new Date(unix_timestamp * 1000); // cause Date() uses non-unixtime for some reason // TODO make more compact?
+		return date.toLocaleString();
+	}
 
-async function unix_timestamp_to_data(unix_timestamp: number) {
-	// Convert UNIX timestamp to human readable format (27/08/2025, 16.00.00)
-	const date = new Date(unix_timestamp * 1000); // cause Date() uses non-unixtime for some reason // TODO make more compact?
-	return date.toLocaleString();
+	public get_now(): number {
+		// Get current unixtime
+		return Number((new Date().getTime() / 1000).toFixed()) // Converting to number because .toFixed() for rouning numbers, returns STRING. WTF.
+	}
 }
+
+const unix_time = new UnixTime()
 
 async function make_request(currency: string) {
 	var request_response = await fetch(`https://api.fxratesapi.com/latest?base=${currency}`)
@@ -71,7 +89,7 @@ async function exchange_rates_UI() {
 	const EUR_data = await currency_controller.get_rates("EUR")
 	const USD_data = await currency_controller.get_rates("USD")
 
-	return_text = return_text + `Server latest refresh: ${await unix_timestamp_to_data(UAH_data.unix_latest_refresh)}\n\n`
+	return_text = return_text + `Server latest refresh: ${await unix_time.to_data(UAH_data.unix_latest_refresh)}\n\n`
 
 	return_text = return_text + "- = Exchange course for UAH = -"
 	return_text = return_text + `\n\nEUR:\nBuy: ${(1 / UAH_data["rates"]["EUR"]).toFixed(2)}\nSell: ${EUR_data["rates"]["UAH"].toFixed(2)}` // TODO
@@ -87,10 +105,11 @@ const currency_controller = new CurrencyController()
 
 async function main() {
 	while (true) {
+
 		await exchange_rates_UI()
 
 		for (let i = 0; i < 21; i++) { // refresh every 20 secs for cache check
-			process.stdout.write(`Waiting ${i}/20 secs\r\r`);
+			process.stdout.write(`\r\rWaiting ${i}/20 secs`);
 			await setTimeout(1000) // ms
 		}
 	}
